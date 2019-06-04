5e1817f0ad451d201414cf247e9e643f928ca075
unknown <hsaidi291@gmail.com>
2019-06-05 00:24:21 +0200

    variables

--- src/main.js
+++ src/main.js
@@ -1,1 +1,54 @@
-let test="hello";
+"use strict"
+
+
+function testVar() {
+
+    var a = 50; console.log(a);
+}
+testVar();
+
+
+for (let i = 0; i < 10; i++) {
+
+    console.log(i);
+}
+
+const colors = [];
+colors.push('red');
+colors.push('blue');
+//colors = ['red', 'green'];
+
+console.log(colors);
+class user {
+    constructor(username, email, pasword) {
+
+
+        this.email = email;
+        this.pasword = pasword;
+        this.username = username;
+        this.money = 30;
+
+    }
+
+    register() {
+
+
+
+        console.log(this.username + " is registred ");
+    }
+
+}
+let User = new user('hamza', 'hamza@', '123');
+User.register();
+class member extends user {
+
+    constructor(username, email, pasword, member) {
+
+        super(username,email,pasword);
+        this.member = member;
+
+
+    }
+}
+let hamouda=new member('hamouda','ha@','AZR','member of est');
+hamouda.register();
---
+++ src/user.js
@@ -0,0 +1,32 @@
+"use strict"
+class user{
+constructor(username,email,pasword){
+
+
+this.email=email;
+this.pasword=pasword;
+this.username=username;
+this.money=30;
+
+}
+
+register(){
+
+
+
+    console.log(username+" is registred ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
